54e48d4fff8516fe16e6aaa29f0cc644351c6d57
johnson365 <515217164@qq.com>
2014-11-27 11:06:27 +0800

    delete
    delete

--- tiger/runtime/control.c
+++
@@ -1,5 +0,0 @@
-#include "control.h"
-
-//int Control_heapSize = 1024;
-
-int Control_heapSize = 500;
--- tiger/src/codegen/bytecode/stm/T.java
+++
@@ -1,6 +0,0 @@
-package codegen.bytecode.stm;
-
-
-public abstract class T implements codegen.bytecode.Acceptable
-{
-}
